3acf38465c38912d1fd74617b866529e465383bd
Vaishnavi Yelisetti <69136006+18WH1A0567@users.noreply.github.com>
2021-06-03 18:25:00 +0530

    add even add series

---
+++ Arrays and strings/EvenOddSeries.java
@@ -0,0 +1,39 @@
+package day.two;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+
+import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
+
+public class EvenOddSeries {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		Set<Integer> set = new HashSet<Integer>();
+		
+		System.out.println("Enter the string: ");
+		String st = scan.nextLine();
+		int l = st.length();
+		
+		for(int i = 0;i < l; i++) {
+			if (Character.isDigit(st.charAt(i))) {
+				set.add(Character.getNumericValue(st.charAt(i)));
+			}
+		}
+		List<Integer> list = new ArrayList<Integer>(set);
+		Collections.sort(list, Collections.reverseOrder());
+		
+		for(Integer i : list) {
+			System.out.print(i);
+		}
+		System.out.println();
+		
+
+	}
+
+}
