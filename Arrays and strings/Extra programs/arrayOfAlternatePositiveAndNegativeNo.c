47d8090e5316f9f5954a514e529165f4ab9226ed
Vaishnavi Yelisetti <69136006+18WH1A0567@users.noreply.github.com>
2021-06-04 15:58:58 +0530

    Commit arrayOfAlternatePositiveAndNegativeNo.c

---
+++ Arrays and strings/Extra programs/arrayOfAlternatePositiveAndNegativeNo.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+    int n, j, k;
+    scanf("%d", &n);
+    int a[n];
+    for(int i = 0;i < n;i++){
+        scanf("%d", &a[i]);
+    }
+    
+    
+    for(int i = 1; i < n;i++){
+        j = i - 1;
+        k = a[i];
+        if(k < 0){
+            while(j >= 0 && a[j] > k && a[j] > 0){
+                a[j+1] = a[j];
+                j -= 1;
+            }
+            a[j+2] = k;
+        }
+        
+    }
+
+    for(int i = 0;i < n;i++){
+        printf("%d ", a[i]);
+    }
+    return 0;
+}
